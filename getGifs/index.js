52ee252fefc22a8460c4039377010c6572801786
Patrali <patrali.de@gmail.com>
2017-07-23 23:27:25 -0600

    Update index.js
    updated to match faker v4.1.0

--- getGifs/index.js
+++ getGifs/index.js
@@ -6,8 +6,8 @@ var gifs = [];
 for(var i = 0; i < 10; i++) {
  var gif = {};
  gif.id = faker.random.number(100);
- gif.title = faker.Lorem.sentences(1);
- gif.url = faker.Image.imageUrl(100, 100, 'sports') + "?" + faker.random.number(200);
+ gif.title = faker.lorem.sentence(1);
+ gif.url = faker.image.imageUrl(100, 100, 'sports') + "?" + faker.random.number(200);
  gifs.push(gif);
  }
  callback(null, gifs);
